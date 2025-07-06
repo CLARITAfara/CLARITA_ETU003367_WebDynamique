cc5d0f6af23eb57a7af8d48e09040dd9dac7d818
CLARITAfara <clarita.razafiarisoa@gmail.com>
2025-07-07 02:54:02 +0300

    useless part
    useless part dans AdminRepository.java

--- src/main/java/gestion/bibliotheque/repository/AdminRepository.java
+++ src/main/java/gestion/bibliotheque/repository/AdminRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    // Optional<Admin> findByNomUtilisateurAndMotDePasse(String nomUtilisateur, String motDePasse);
 
     Optional<Admin> findByNomUtilisateur(String nomUtilisateur);
     Optional<Admin> findByMotDePasse(String motDePasse);
